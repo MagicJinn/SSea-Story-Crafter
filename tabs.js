9d1e0f3cbd93fce87ddb5d23e2c1dae37f2146b3
MagicJinn <jinn.bz@mail.com>
2023-03-14 21:45:43 +0100

    created a tab selection function

---
+++ tabs.js
@@ -0,0 +1,22 @@
+function Tabs() {
+    textAlign(CENTER, CENTER)
+    textSize(16)
+    let tabs = ["Docs", "Qualities", "Story", ""]
+    for (let i = 0; i < tabs.length; i++) {
+        if (!currentTab[i]) {
+            currentTab[i] = Button({
+                x: 70 + (tab.width + 12) * i,
+                y: 25,
+                buttonText: tabs[i],
+                img: tab
+            })
+            if (currentTab[i]) {
+                for (let i2 = 0; i2 < tabs.length; i2++) {
+                    if (i2 !== i) {
+                        currentTab[i2] = false
+                    }
+                }
+            }
+        }
+    }
+}
