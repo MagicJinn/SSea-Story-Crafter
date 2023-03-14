34fa4a5842d29612d427b42eaf55f42df28ef386
MagicJinn <jinn.bz@mail.com>
2023-03-14 21:46:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-function setup(){
+let currentTab = []
+
+function setup() {
+    createCanvas(book.width, book.height + tab.height)
+    Scaling()
+    textFont(Fontin)
+    noSmooth()
+
+    root = createCheckbox("Event is root?")
     title = createInput("Title (mandatory)")
+    storyImage = createInput("Image for your story, without extension")
+    teaser = createInput("Teaser (only applicable for non root)")
+
+    image(book, 0, 0 + tab.height)
+}
+
+function draw() {
+    if (root) {}
+    resizeInput(title);
+    resizeInput(storyImage);
+    resizeInput(teaser);
+    imageMode(CORNER)
+    image(pages, 0, 0 + tab.height)
+    Tabs()
+    if (currentTab[0]) {} else if (currentTab[1]) {}
+}
+
+function resizeInput(input) {
+    input.size(input.value().length * 7);
 }
