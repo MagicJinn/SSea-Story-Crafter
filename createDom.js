1b4c8c5c03223f01da688fe9866e6267d8d7b83b
MagicJinn <jinn.bz@mail.com>
2023-03-14 22:23:38 +0100

    Create createDom.js

---
+++ createDom.js
@@ -0,0 +1,6 @@
+function CreateDom() {
+    root = createCheckbox("Event is root?")
+    title = createInput("Title (mandatory)")
+    storyImage = createInput("Image for your story, without extension")
+    teaser = createInput("Teaser (only applicable for non root)")
+}
