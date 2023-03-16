7500556fd354b1f6f6a28384116c55cd3ac741d9
MagicJinn <jinn.bz@mail.com>
2023-03-16 15:34:30 +0100

    Delete createDom.js

--- createDom.js
+++
@@ -1,6 +0,0 @@
-function CreateDom() {
-    root = createCheckbox("Event is root?")
-    title = createInput("Title (mandatory)")
-    storyImage = createInput("Image for your story, without extension")
-    teaser = createInput("Teaser (only applicable for non root)")
-}
