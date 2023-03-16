f0b1082d18a245d58576816eea92592fa2902bf1
MagicJinn <jinn.bz@mail.com>
2023-03-16 17:17:29 +0100

    added new images

--- preload.js
+++ preload.js
@@ -1,10 +1,15 @@
 function preload() {
     let p = "images/" //Path
-    let e = ".png" // extension
+    let e = ".png" // Extension
     book = loadImage(p + "book" + e)
     pages = loadImage(p + "pages" + e)
     story = loadImage(p + "story" + e)
     tab = loadImage(p + "tab" + e)
+    frame = loadImage(p + "frame" + e)
+
+    gitAvatar = loadImage(p + "gitAvatar" + e)
+    gitLogo = loadImage(p + "logos/gitLogo" + e)
+
 
     Fontin = loadFont("styles/Fontin-SmallCaps.ttf") // http://eljbris.com/fontin.html
 }
