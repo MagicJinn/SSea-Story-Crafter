3e9b2bbcefa274bdcb3e35279f771041249e3593
MagicJinn <jinn.bz@mail.com>
2023-03-18 19:08:15 +0100

    worked on DOM features

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let currentTab = []
+let previousTab = []
 
 function setup() {
     createCanvas(book.width, book.height + tab.height)
@@ -6,12 +7,42 @@ function setup() {
     noSmooth()
     UI(SETUP)
 }
+var domSetup = false
 
 function draw() {
     UI(DRAW)
     if (currentTab[0]) {
         InfoTab()
     } else if (currentTab[1]) {
+        // if (!domSetup) {
+        //     let button = []
+        //     let div = []
+        //     for (let i = 0; i < buttonAmount; i++) {
+        //         button[i] = createButton("test")
+        //         button[i].mousePressed(function () {
+        //             saveQuality({
+        //                 OwnerName: "MyMod",
+        //                 Description: "This is a new quality",
+        //                 Image: "new_quality_icon",
+        //                 Name: "New Quality",
+        //                 Id: 6000000
+        //             })
+        //         })
+        //         if (i % 2 == 1) {
+        //             div[i] = createDiv("Another one")
+        //         }
+        //         domSetup = true
+        //     }
+        // }
+    }
+}
 
+let buttonAmount = 1
+
+function mouseClicked() {
+    if (currentTab[1] == true) {
+        domSetup = false
+        buttonAmount++
+        removeElements()
     }
 }
