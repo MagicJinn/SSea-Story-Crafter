d6a02fe1b415a6128d28d450e902ceef04a04ec1
MagicJinn <jinn.bz@mail.com>
2023-03-18 15:57:04 +0100

    added placeholder images

--- preload.js
+++ preload.js
@@ -1,5 +1,3 @@
-var Fontin
-
 function preload() {
     let p = "images/" //Path
     let e = ".png" // Extension
@@ -8,6 +6,13 @@ function preload() {
     story = loadImage(p + "story" + e)
     tab = loadImage(p + "tab" + e)
     frame = loadImage(p + "frame" + e)
+
+    let cms = "comingsoon/comingsoon"
+    placeholder = {
+        small: loadImage(p + cms + "small" + e),
+        default: loadImage(p + cms + e),
+        gaz: loadImage(p + cms + "gaz" + e)
+    }
 
     gitAvatar = loadImage(p + "logos/gitAvatar" + e) // http://avatars.githubusercontent.com/u/72989508
     gitLogo = loadImage(p + "logos/gitLogo" + e) // http://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png
