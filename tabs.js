24ab413fa17b25caaf934a5b5050c3b13ddef886
MagicJinn <jinn.bz@mail.com>
2023-04-01 21:37:43 +0200

    Optimised code, added heavily expanded implemented inputs

--- tabs.js
+++ tabs.js
@@ -2,8 +2,8 @@ function Tabs() {
     textAlign(CENTER, CENTER)
     textSize(16)
     textFont(Fontin.SmallCaps)
-    let tabs = ["Info", "Qualities", "Stories"]
-    let buttonWidth = 1.2
+    const tabs = ["Info", "Qualities", "Stories"]
+    const buttonWidth = 1.2
     for (let i = 0; i < tabs.length; i++) {
         if (!currentTab[i]) {
             currentTab[i] = Button({
@@ -26,15 +26,9 @@ function Tabs() {
 }
 
 function InfoTab() {
-    imageGaz(paperstack)
-
-    textAlign(LEFT)
-    textSize(26)
-    textFont(Fontin.SmallCaps)
-    text("Information", 420, 60)
-    textSize(15)
-    textFont(Fontin.Regular)
-    text("Useful information to get you started, learn more\nabout the project, or contribute.", 425, 95)
+    ImageGaz(paperstack)
+    PageTitle("Information")
+    PageDescription("Useful information to get you started, learn more about the project, or contribute.")
 
     let h = "http://"
     let g = "github.com/"
@@ -63,7 +57,8 @@ function InfoTab() {
             img: Story({
                 img: buttons[i].image,
                 title: buttons[i].title,
-                teaser: buttons[i].teaser
+                teaser: buttons[i].teaser,
+                id: i
             }),
             url: buttons[i].url,
             tintColor: color(255, 225, 0)
@@ -82,7 +77,12 @@ var quality = {
     UsePyramidNumbers: false,
     PyramidNumberIncreaseLimit: 50,
     AvailableAt: null,
-    Ordering: 0
+    Ordering: 0,
+    IsSlot: false,
+    LimitedToArea: null,
+    Persistent: false,
+    Nature: "Unspecified",
+    Category: "Unspecified",
 }
 var meta = {
     advancedMode: false, // Whether advancedMode is activated
@@ -90,10 +90,12 @@ var meta = {
 }
 let refresh = false
 let save = false
-function QualityTab() {
-    if (!domSetup) { // Create all buttons.
-        advancedMode = createCheckbox("🔧 Advanced Mode", meta.advancedMode)
 
+function QualityTab() {
+    ImageGaz()
+    PageTitle()
+    PageDescription()
+    if (!domSetup) { // Create all buttons.
         createSpan("Id: ")
         Id = createInput(quality.Id)
         createDiv()
@@ -110,6 +112,45 @@ function QualityTab() {
         Image = createInput(quality.Image)
         createDiv()
 
+        createSpan()
+        Persistent = createCheckbox("Persistent", quality.Persistent)
+        createDiv()
+
+        createSpan("Nature: ")
+        Nature = createSelect()
+        Nature.option("Unspecified")
+        Nature.option("Status")
+        Nature.option("Thing")
+        Nature.selected(quality.Nature)
+        createDiv()
+
+        if (quality.Nature == "Status") {
+            createSpan("Category: ")
+            CategoryStatus = createSelect()
+            CategoryStatus.option("Unspecified")
+            CategoryStatus.option("Circumstance")
+            CategoryStatus.option("Story")
+            CategoryStatus.option("Progress")
+            CategoryStatus.option("Quest")
+            CategoryStatus.option("Accomplishment")
+            CategoryStatus.option("BasicAbility")
+            CategoryStatus.option("SpecificAbility")
+            CategoryStatus.option("MinorLateral")
+            CategoryStatus.selected(quality.Category)
+            createDiv()
+        } else if (quality.Nature == "Thing") {
+            createSpan("Category: ")
+            CategoryThing = createSelect()
+            CategoryThing.option("Unspecified")
+            CategoryThing.option("Companion")
+            CategoryThing.option("Goods")
+            CategoryThing.option("Ship")
+            CategoryThing.option("Curiosity")
+            CategoryThing.selected(quality.Category)
+            createDiv()
+        }
+
+        advancedMode = createCheckbox("🔧 Advanced Mode", meta.advancedMode)
         if (meta.advancedMode) {
             createSpan("🔧 Notes: ")
             Notes = createInput(quality.Notes)
@@ -126,8 +167,9 @@ function QualityTab() {
             createSpan()
             UsePyramidNumbers = createCheckbox("🔧 UsePyramidNumbers", quality.UsePyramidNumbers)
             createDiv()
+
             if (quality.UsePyramidNumbers) {
-                createSpan("🔧PyramidNumberIncreaseLimit: ")
+                createSpan("└🔧PyramidNumberIncreaseLimit: ")
                 PyramidNumberIncreaseLimit = createInput(quality.PyramidNumberIncreaseLimit)
                 createDiv()
             }
@@ -138,6 +180,21 @@ function QualityTab() {
 
             createSpan("🔧Ordering: ")
             Ordering = createInput(quality.Ordering)
+            createDiv()
+
+            createSpan("🔧LimitedToArea: ")
+            LimitedToArea = createInput(quality.LimitedToArea)
+            createDiv()
+
+            createSpan()
+            IsSlot = createCheckbox("🔧IsSlot", quality.IsSlot)
+            createDiv()
+
+            if (quality.IsSlot) {
+                createSpan("└🔧AssignToSlot")
+                AssignToSlot = createInput(quality.AssignToSlot)
+                createDiv()
+            }
         }
 
         // Button to create the Json
@@ -151,14 +208,34 @@ function QualityTab() {
     }
 
     // Check for changes that require a refresh
+    Persistent.changed(function () {
+        quality.Persistent = !quality.Persistent
+    })
+    Nature.changed(function () {
+        refresh = true
+    })
+    if (Nature == "Status") {
+        CategoryStatus.changed(function () {
+            refresh = true
+        })
+    } else if (Nature == "Thing") {
+        CategoryThing.changed(function () {
+            refresh = true
+        })
+    }
+
     advancedMode.changed(function () {
-        meta.advancedMode = !meta.advancedMode // Switches the boolean for the checkbox
+        meta.advancedMode = !meta.advancedMode
         refresh = true
     })
     if (meta.advancedMode) {
         try {
             UsePyramidNumbers.changed(function () {
                 quality.UsePyramidNumbers = !quality.UsePyramidNumbers
+                refresh = true
+            })
+            IsSlot.changed(function () {
+                quality.IsSlot = !quality.IsSlot
                 refresh = true
             })
         } catch (error) {
@@ -171,6 +248,13 @@ function QualityTab() {
         quality.Name = Name.value()
         quality.Description = Description.value()
         quality.Image = Image.value()
+        quality.Nature = Nature.value()
+        if (Nature == "Status") {
+            quality.Category = CategoryStatus.value()
+        } else if (Nature == "Thing") {
+            quality.Category = CategoryThing.value()
+        }
+
         if (meta.advancedMode) {
             try {
                 quality.Notes = Notes.value()
@@ -180,6 +264,9 @@ function QualityTab() {
                 if (quality.UsePyramidNumbers) {
                     quality.PyramidNumberIncreaseLimit = PyramidNumberIncreaseLimit.value()
                 }
+                if (quality.IsSlot) {
+                    quality.AssignToSlot = AssignToSlot.value()
+                }
                 quality.AvailableAt = AvailableAt.value()
                 quality.Ordering = Ordering.value()
             } catch (error) {
@@ -187,7 +274,7 @@ function QualityTab() {
             }
         }
 
-        refreshDom() // Refresh all DOMs
+        RefreshDom() // Refresh all DOMs
         refresh = false
     }
 
@@ -195,4 +282,10 @@ function QualityTab() {
         SaveQuality(quality)
         save = false
     }
+}
+
+function StoryTab() {
+    ImageGaz(placeholder.gaz)
+    PageTitle("Coming Soon...")
+    PageDescription("Nothing awaits you. Time to leave port?")
 }
